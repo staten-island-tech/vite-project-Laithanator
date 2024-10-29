c994efddc85b9636cbc329bbf7b9254ad0984739
Laithanator <144480666+Laithanator@users.noreply.github.com>
2024-10-29 13:58:48 -0400

    gweg43g

---
+++ viteProject/JS/itemArray.js
@@ -0,0 +1,62 @@
+const RRCharacters = [
+  {
+    movieName: "Deadpool",
+    characterName: "Wade Wilson / Deadpool",
+    imgLink: "",
+    alt: "Ryan Reynolds as Deadpool in Deadpool",
+  },
+  {
+    movieName: "Free Guy",
+    characterName: "Guy",
+    imgLink: "",
+    alt: "Ryan Reynolds as Guy in Free Guy",
+  },
+  {
+    movieName: "The Hitman's Bodyguard",
+    characterName: "Michael Bryce",
+    imgLink: "",
+    alt: "Ryan Reynolds as Michael Bryce in The Hitman's Bodyguard",
+  },
+  {
+    movieName: "6 Underground",
+    characterName: "One",
+    imgLink: "",
+    alt: "Ryan Reynolds as One in 6 Underground",
+  },
+  {
+    movieName: "The Proposal",
+    characterName: "Andrew Paxton",
+    imgLink: "",
+    alt: "Ryan Reynolds as Andrew Paxton in The Proposal",
+  },
+  {
+    movieName: "Red Notice",
+    characterName: "Nolan Booth",
+    imgLink: "",
+    alt: "Ryan Reynolds as Nolan Booth in Red Notice",
+  },
+  {
+    movieName: "Detective Pikachu",
+    characterName: "Detective Pikachu",
+    imgLink: "",
+    alt: "Ryan Reynolds as Detective Pikachu in Detective Pikachu",
+  },
+  {
+    movieName: "Buried",
+    characterName: "Paul Conroy",
+    imgLink: "",
+    alt: "Ryan Reynolds as Paul Conroy in Buried",
+  },
+  {
+    movieName: "The Croods",
+    characterName: "Guy (Voice)",
+    imgLink: "",
+    alt: "Ryan Reynolds as Guy in The Croods",
+  },
+  {
+    movieName: "Van Wilder: Party Liaison",
+    characterName: "Van Wilder",
+    imgLink: "",
+    alt: "Ryan Reynolds as Van Wilder in Van Wilder: Party Liaison",
+  },
+];
