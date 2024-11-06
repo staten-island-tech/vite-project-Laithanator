3933d06cd7d6083d76738e17a4fc57d0ad53b051
Laithanator <144480666+Laithanator@users.noreply.github.com>
2024-11-06 13:58:32 -0500

    fwege4whgh

--- viteProject/JS/itemArray.js
+++ viteProject/JS/itemArray.js
@@ -70,3 +70,5 @@ const RRCharacters = [
     shirtless: false,
   },
 ];
+
+export { RRCharacters };
--- viteProject/JS/main.js
+++ viteProject/JS/main.js
@@ -1,1 +1,22 @@
 import "../CSS/style.css";
+import { RRCharacters } from "../JS/itemArray";
+
+const DOMSelectors = {
+  container: document.querySelector(".container"),
+};
+
+let idCounter = 0;
+
+function start() {
+  RRCharacters.forEach((card) => {
+    idCounter += 1;
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeEnd",
+      `<div class="card" id="card-${idCounter}">
+    <h2 class="card-heading" id="heading-${idCounter}">${card.characterName}</h2>
+    <h3 class="card-subheading" id="heading-${idCounter}">${card.movieName}</h3>
+    <img class="card-img" id="img-${idCounter}" src="${card.imgLink}" alt="${card.alt}"></div>`
+    );
+  });
+}
+start();
